b57604428f18090140bd07fa2093cf8c5ca6fa33
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-05 10:44:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  var result = document.querySelector(`${selector}`);
+  return result;
+}
+function nestedTarget() {
+  var tofind = document.getElementById('nested').querySelector('div div div div div.target');
+  return tofind;
+}
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + parseInt(n);
+  }
+}
+function deepestChild() {
+  var current = document.getElementById('grand-node')
+  
+  while (current.hasChildNodes() && current.querySelector('div') !== null) {
+      current = current.querySelector('div') 
+  }
+  return current;
+}
