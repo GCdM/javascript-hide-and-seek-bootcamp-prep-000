36657da402d3dac2e04731bb286358bba34254fc
GCdM <gabrielcm.br@gmail.com>
2018-07-17 10:27:06 +0100

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 function getFirstSelector(selector) {
-  var result = document.querySelector(`${selector}`);
-  return result;
+  return document.querySelector(selector);
 }
+
 function nestedTarget() {
-  var tofind = document.getElementById('nested').querySelector('div div div div div.target');
-  return tofind;
+  return document.querySelector('#nested div div div .target');
 }
+
 function increaseRankBy(n) {
-  var lis = document.querySelectorAll('ul.ranked-list li');
-  for (var i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML) + parseInt(n);
-  }
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  lis.forEach(li => li.innerHTML = parseInt(li.innerHTML) + parseInt(n));
 }
+
 function deepestChild() {
-  var current = document.getElementById('grand-node')
-  
-  while (current.hasChildNodes() && current.querySelector('div') !== null) {
-      current = current.querySelector('div') 
+  let element = document.querySelector('#grand-node');
+
+  while (element.querySelector('div') !== null) {
+    element = element.querySelector('div');
   }
-  return current;
+
+  return element;
 }
+
+
+
+
+
+
+// function getFirstSelector(selector) {
+//   var result = document.querySelector(`${selector}`);
+//   return result;
+// }
+// function nestedTarget() {
+//   var tofind = document.getElementById('nested').querySelector('div div div div div.target');
+//   return tofind;
+// }
+// function increaseRankBy(n) {
+//   var lis = document.querySelectorAll('ul.ranked-list li');
+//   for (var i = 0; i < lis.length; i++) {
+//     lis[i].innerHTML = parseInt(lis[i].innerHTML) + parseInt(n);
+//   }
+// }
+// function deepestChild() {
+//   var current = document.getElementById('grand-node')
+//
+//   while (current.hasChildNodes() && current.querySelector('div') !== null) {
+//       current = current.querySelector('div')
+//   }
+//   return current;
+// }
